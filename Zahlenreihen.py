c46ddf13e908baaf72b5ca4c601a279051e50a64
Ninjaishyper <171witb12@o365.htl-leoben.at>
2018-04-18 12:05:34 +0200

    Zahlenreihen-mit-geraden-ungeraden.py

--- Zahlenreihen.py
+++ Zahlenreihen.py
@@ -19,7 +19,12 @@ print()
 for i in range (1,n+1):
     if (i % 2):
         print (i, end = " ")
-  
-
-
+print()
+print()
 # alle Zahlen von 1 bis n die sich /9 teilen lassen ausgebn
+for i in range (1,n+1):
+    if (i % 9 == 0):
+        if(i + 1 != n):
+            print (i, end = " ")
+        else:
+            print (i)
