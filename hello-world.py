9a43f75d59675dcae960ac95784fbcf6238c85d7
Ninjaishyper <171witb12@o365.htl-leoben.at>
2018-04-18 10:15:53 +0200

    hello-world erweiterung

--- hello-world.py
+++ hello-world.py
@@ -7,5 +7,9 @@ print("Hallo schön dich zu sehen" , name, "!")
 print ("dein Name ist",len(name),"Zeichen lang!")
 
 alter = input("Wie alt bist du")
-
-print ("In einem Jahr wirst du",int(alter)+1,".")
+alter = int(alter)
+if(alter > 100):
+    print("Das ist wohl ein Scherz- oder :-)")
+else:
+    print ("In einem Jahr wirst du",int(alter)+1,".")
+print("Tschüss...")
